9b5a4d512c6b4925eeffcf63628301cab45666a5
Crindzebra <crs_16423@outlook.com>
2018-02-19 19:28:29 +0800

    add drawer items

--- js/main.js
+++ js/main.js
@@ -1,2 +1,51 @@
-// JavaScript Document
 //南无阿弥陀佛 佛祖保佑无bug
+	function drawerclick()
+	{
+		var maindiv=getElementById("main");
+		if(maindiv.className=="")
+			{
+				maindiv.className="div-draweropen";
+			}else{
+				maindiv.className="";
+			}
+		var drawerdiv=getElementById("drawer");
+		if(drawerdiv.hidden=="hidden")
+			{
+				drawerdiv.removeAttribute("hidden");
+			}else{
+				maindiv.setAttribute("hidden","hidden");
+			}
+	}//南无阿弥陀佛 佛祖保佑无bug
+	function drawerclick()
+	{
+		var maindiv=getElementById("main");
+		if(maindiv.className=="")
+			{
+				maindiv.className="div-draweropen";
+			}else{
+				maindiv.className="";
+			}
+		var drawerdiv=getElementById("drawer");
+		if(drawerdiv.hidden=="hidden")
+			{
+				drawerdiv.removeAttribute("hidden");
+			}else{
+				maindiv.setAttribute("hidden","hidden");
+			}
+	}//南无阿弥陀佛 佛祖保佑无bug
+	function drawerclick()
+	{
+		var maindiv=getElementById("main");
+		if(maindiv.className=="")
+			{
+				maindiv.className="div-draweropen";
+			}else{
+				maindiv.className="";
+			}
+		var drawerdiv=getElementById("drawer");
+		if(drawerdiv.hidden=="hidden")
+			{
+				drawerdiv.removeAttribute("hidden");
+			}else{
+				maindiv.setAttribute("hidden","hidden");
+			}// JavaScript Document
