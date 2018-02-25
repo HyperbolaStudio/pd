2f391882a6bb9cda2b27e0ad0a486b7892a795c2
Crindzebra <crs_16423@outlook.com>
2018-02-25 19:42:19 +0800

    a fucking reeeeee and add some fucking components and re some names

--- en_US/js/main.js
+++ en_US/js/main.js
@@ -1,29 +1,29 @@
 // JavaScript Document
 //南无阿弥陀佛 佛祖保佑无bug
-$(document).ready(function(){
+function closedrawer(){
 	$("#mask").hide();
-	$("#drawer").animate({left:"-300px"},500);
-	$("#mask").click(function(){
-		$("#mask").hide();
-		$("#drawer").animate({left:"-300px"},500);
-		setTimeout("$('#drawer').hide()",500);
-	});
-	$(".drawer-title").click(function(){
-		$("#mask").hide();
-		$("#drawer").animate({left:"-300px"},500);
-		$("#drawer").hide();
-		setTimeout("$('#drawer').hide()",500);
-	});
-	$(".drawer-item").click(function(){
-		$("#mask").hide();
-		$("#drawer").animate({left:"-300px"},500);
-		$("#drawer").hide();
-		setTimeout("$('#drawer').hide()",500);
-	});
-	$("#dwclick").click(function(){
-		$("#mask").show();
+	$("#drawer").animate({left:"-300px"},500,$('#drawer').hide());
+}
+function opendrawer(){
+	$("#mask").show();
 		$("#drawer").animate({left:"-270px"},0);
 		$("#drawer").show();
 		$("#drawer").animate({left:"0px"},500);
+}
+$(document).ready(function(){
+	$("#mask").click(function(){
+		closedrawer();
+	});
+	$(".drawer-title").click(function(){
+		closedrawer();
+	});
+	$(".drawer-item").click(function(){
+		closedrawer();
+	});
+	$("#dwclick").click(function(){
+		opendrawer()
+	});
+	$("#backclick").click(function(){
+		history.back();
 	});
 });
